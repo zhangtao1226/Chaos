22c926881ece2d72e65740051e0db9326adf13d9
zhangtao <zhangtao1226@outlook.com>
2022-10-20 10:45:41 +0800

    时间对齐

---
+++ 时间对齐/TimeAligning.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author  : zhangtao
+# @File    : TimeAligning.py
+# @Desc    : 时间对齐
+# @Time    : 2022/10/20 10:38
+# @Software: PyCharm
+
+import pandas as pd
+import numpy as np
+
+class TimeAligning:
+    """
+    数据项时间对齐，可允许设置范围差
+    """
+    def __init__(self):
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    pass
